e43ef0d81b44829f6c3d982cbf9ff0abf3739974
developerdiary <ikhlaquesoft@gmail.com>
2022-09-13 17:12:10 +0530

    On Change Filter

--- src/Components/ListPosts.js
+++ src/Components/ListPosts.js
@@ -1,48 +1,50 @@
 import React, { useState, useEffect } from "react";
 
 const ListPosts = () => {
- const [search, setSearch] = useState("")
- const [post, setPost] = useState([])
- const [filterPost, setFilterPost] = useState([])
-  
+  const [search, setSearch] = useState("")
+  const [loading, setLoading] = useState(false);
+  const [post, setPost] = useState([])
+  const [filterPost, setFilterPost] = useState([])
+
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
+    setLoading(true);
     fetchData();
   }, []);
 
-  const fetchData = async() => {
+  const fetchData = async () => {
     let response = await fetch("https://jsonplaceholder.typicode.com/posts")
-    let data = await response.json()  
-    setPost(data)  
+    let data = await response.json()
+    setPost(data)
     setFilterPost(data)
+    setLoading(false);
   };
 
-  const searchPost = () =>{
-    if(search){
-        let newPosts = post.filter( (item) => {
-            return item.title == search
-        })
-        setFilterPost(newPosts)
+  const searchPost = (searchValue) =>{
+    setSearch(searchValue)
+    if (searchValue !== "") {
+      let newSearchData = post.filter((value) => {
+          return value.title.toLowerCase().includes(searchValue.toLowerCase())
+      })
+      setFilterPost(newSearchData)
     }else{
-        setFilterPost(post)
+      setFilterPost(post)
     }
     
   }
 
+  
   return (
     <div>
-        <h1>Post</h1>
-        <input type="text" value={search} onChange={ (e) => setSearch(e.target.value)} /> <button onClick={() => searchPost()}> Search </button>
-        <ul>
-            {
-                filterPost.map( (item) => {
-                    return (
-                        <li> {item.title} </li>
-                    )
-                })
-                
-            }
-        </ul>
+      <h1>Post</h1>
+      <input type="text" value={search} onChange={(e) => searchPost(e.target.value)} /> 
+      <ul>
+      {loading ? (
+        <h4>Loading ...</h4>
+      ) : (
+        filterPost.map((item) => <h5 key={item.id}>{item.title}</h5>)
+      )}
+      </ul>
     </div>
   );
 };
